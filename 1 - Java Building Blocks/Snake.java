451f7224f4e249ee4832297f76752c80030340cd
Max Schwaab <max.schwaab@westernacher.com>
2015-05-11 22:29:16 +0200

    added Snake.java

---
+++ 1 - Java Building Blocks/Snake.java
@@ -0,0 +1,11 @@
+public class Snake {
+	// String result = "done";
+	public void shed(boolean time) {
+		// String result = "done";
+		if(time) {
+			// String result = "done";
+		}
+		System.out.println(result);
+		// String result = "done";
+	}
+}

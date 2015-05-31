853e5831e4f7ae0aa90a83c2dc5b182e73a3f917
Philipp Joe Liepert <philipp.liepert@westernacher.com>
2015-05-31 19:12:35 +0200

    add champter 1, questions 8-16

---
+++ 1 - Java Building Blocks/Question 16/Price.java
@@ -0,0 +1,53 @@
+//Question A
+public class Price {
+
+	public void admission(){
+		int amount = 9L;
+		System.out.println(amount);
+	}
+}
+
+//Question B
+/*public class Price {
+
+	public void admission(){
+		int amount = 0b101;		
+		System.out.println(amount);
+	}
+}*/
+
+//Question C
+/*public class Price {
+
+	public void admission(){
+		int amount 0xE;
+		System.out.println(amount);
+	}
+}*/
+
+//Question D
+/*public class Price {
+
+	public void admission(){
+		double amount 0xE;
+		System.out.println(amount);
+	}
+}*/
+
+//Question E
+/*public class Price {
+	
+	public void admission(){
+		double amount = 1_2_.0_0;
+		System.out.println(amount);
+	}
+}*/
+
+//Question F
+/*public class Price {
+
+	public void admission(){
+		int amount 1_2_;
+		System.out.println(amount);
+	}
+}*/

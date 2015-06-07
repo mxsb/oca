040277b4a040f51cbfc7f107b1ed7803eb227454
Benjamin Haupt <info@haupt-itservices.de>
2015-06-07 11:18:32 +0200

    Chapter 2 - Operators and Statements
    - Question 1-7

---
+++ Test.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Benjamin Haupt on 19.05.15.
+ */
+public class Test {
+
+    {
+        System.out.println("nochmal");
+    }
+
+    public static void main(String[] args) {
+        {
+            System.out.println("xyz");
+        }
+
+        float f = 5.0f;
+
+    }
+
+
+}
